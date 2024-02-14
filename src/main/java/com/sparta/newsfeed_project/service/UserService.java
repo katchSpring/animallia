0db58656790bb8ae6d8204b0210da03b80da8535
onenyeong <a48142826@gmail.com>
2024-02-14 19:22:10 +0900

    profile 비밀번호 체크 로직 일부 수정

--- src/main/java/com/sparta/newsfeed_project/service/UserService.java
+++ src/main/java/com/sparta/newsfeed_project/service/UserService.java
@@ -5,7 +5,6 @@ import com.sparta.newsfeed_project.dto.SignupRequestDto;
 import com.sparta.newsfeed_project.entity.User;
 import com.sparta.newsfeed_project.entity.UserRoleEnum;
 import com.sparta.newsfeed_project.jwt.JwtUtil;
-import com.sparta.newsfeed_project.jwt.RefreshToken;
 import com.sparta.newsfeed_project.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +29,7 @@ public class UserService {
 
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
-//    private final RefreshTokenRepository refreshTokenRepository;
+    //    private final RefreshTokenRepository refreshTokenRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
@@ -41,7 +40,7 @@ public class UserService {
         String username = requestDto.getUsername();
         String password = passwordEncoder.encode(requestDto.getPassword());
         UserRoleEnum role = requestDto.getRole();
-        userRepository.save(new User(username, password,role));
+        userRepository.save(new User(username, password, role));
     }
 
     @Transactional
@@ -52,20 +51,19 @@ public class UserService {
         User user = userRepository.findByUsername(username).orElseThrow(() ->
                 new IllegalArgumentException("존재하지 않는 username 입니다."));
 
-        if (!passwordEncoder.matches(password,user.getPassword())) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
         String token = jwtUtil.createToken(user.getUsername(), user.getRole());
 //        String refreshToken = jwtUtil.createRefreshToken(secretKey);
-        jwtUtil.addJwtToHeader(token,res);
+        jwtUtil.addJwtToHeader(token, res);
 //        jwtUtil.addJwtToHeader(refreshToken,res);
 //        //레디스에 저장 Refresh 토큰을 저장한다. (사용자 기본키 Id, refresh 토큰, access 토큰 저장)
 //        refreshTokenRepository.save(new RefreshToken(String.valueOf(user.getUserId()), refreshToken, token));
     }
 
 
-
-    public User getUser(Long id){
+    public User getUser(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(IllegalArgumentException::new);
     }
@@ -74,23 +72,23 @@ public class UserService {
     //프로필 수정
     @Transactional
     public UserResponseDto updateProfile(Long id, UserRequestDto dto) {
-        User user = checkPWAndGet(id, dto.getPassword());
+        User user = checkPWAndGet(id,dto);
 
-        user.setPassword(dto.getPassword());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
         user.setIntro(dto.getIntro());
 
         return new UserResponseDto(user);
 
     }
 
-//    //프로필 비밀번호 체크
-    private User checkPWAndGet(Long id, String password) {
+    //    //프로필 비밀번호 체크
+    private User checkPWAndGet(Long id,UserRequestDto dto) {
         User user = getUser(id);
 
-
+        String decodedPassword = dto.getPassword();
         // 비밀번호 체크
-        if (user.getPassword() != null
-                && !Objects.equals(user.getPassword(), password)) {
+        if (decodedPassword != null &&
+                passwordEncoder.matches(decodedPassword, passwordEncoder.encode(user.getPassword()))) {
             throw new IllegalArgumentException();
         }
         return user;
